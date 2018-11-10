9f655a6c1fb6624f204aeda475a8d15206f74191
littlelookre2 <connorfor@gmail.com>
2018-11-09 22:39:03 -0500

    ususal test case, empty list for mean

--- statTools.py
+++ statTools.py
@@ -1,3 +1,6 @@
 def avgMean(mylist):
+    if len(mylist) == 0:
+        raise ValueError("Illegal empty list")
+    pass
     mean = sum(mylist) / len(mylist)
     return mean
--- test_statTools.py
+++ test_statTools.py
@@ -5,3 +5,7 @@ def test_mean_basic1():
     assert(avgMean([10, 20, 30, 40, 50]) == 30)
 def test_mean_basic2():
     assert(avgMean([5, 10, 15, 50]) == 20)
+def test_mean_empty():
+    with pytest.raises(ValueError) as errmsg:
+        avgMean([])
+    assert("Illegal empty list" == str(errmsg.value))
