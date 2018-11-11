1b4d9d254fd60a6d91d72eb267b102475df48378
littlelookre2 <connorfor@gmail.com>
2018-11-11 16:20:55 -0500

    comments for functions mean, variance , and median

--- statTools.py
+++ statTools.py
@@ -1,18 +1,24 @@
 def avgMean(meanlist):
+    #returns error if there is an empty list
     if len(meanlist) == 0:
         raise ValueError("Illegal empty list")
     pass
+    #calculates mean
     mean = sum(meanlist) / len(meanlist)
     return mean
 
 def avgmedian(medianlist):
+    #retunrs error for empty list
     if len(medianlist) == 0:
         raise ValueError("Illegal empty list")
     pass
+    #sorts list
     medianlist.sort()
+    #calculates median for odd amount of numbers in list
     if len(medianlist) % 2 == 1:
         middle = int(len(medianlist) / 2)
         median = medianlist[middle]
+    #calculates median for even amount of numbers in list
     else:
         middle1 = int(len(medianlist) / 2)
         middle2 = middle1 - 1
@@ -22,12 +28,15 @@ def avgmedian(medianlist):
 
 def avgvariance(variancelist):
     count = 0
+    #returns error for an empty list
     if len(variancelist) == 0:
         raise ValueError("Illegal empty list")
     pass
+    #returns error if the mean is negative
     if avgMean(variancelist) < 0:
         raise ValueError("Illegal negative mean")
     pass
+    #calculates variance
     for i in range(len(variancelist)):
         count += (avgMean(variancelist) - (variancelist[i]))**2
     variance = count / len(variancelist)
