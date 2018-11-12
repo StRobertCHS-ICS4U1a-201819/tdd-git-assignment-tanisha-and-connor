ea8d6595cb5ea16c0af045af5a9a25f60485bba2
littlelookre2 <connorfor@gmail.com>
2018-11-11 19:58:02 -0500

    test for standard deviation empty list and corner case

--- test_statTools.py
+++ test_statTools.py
@@ -71,4 +71,10 @@ def test_standarddev_basic1():
     assert (deviation([45, 20, 15, 75, 100]) == 32.46536616149585)
 def test_standarddev_basic2():
     assert (deviation([0, 20, 40, 20]) == 14.142135623730951)
+def test_standarddev_empty():
+    with pytest.raises(ValueError) as errmsg:
+        deviation([])
+    assert ("Illegal empty list" == str(errmsg.value))
+def test_standarddev_corner():
+    assert(deviation([0]) == 0)
 
